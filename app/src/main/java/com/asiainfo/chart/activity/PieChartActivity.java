12606654bdc6744d43e70d7a4f88c52e3c7e837e
MicroKibaco <569015640@qq.com>
2017-01-18 00:15:43 +0800

    提交代码

--- app/src/main/java/com/asiainfo/chart/activity/PieChartActivity.java
+++ app/src/main/java/com/asiainfo/chart/activity/PieChartActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.asiainfo.chart.bean.MonthBean;
 import com.asiainfo.chart.chartexample.R;
@@ -15,8 +17,18 @@ import com.asiainfo.chart.fragment.PieFragment;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PieChartActivity extends AppCompatActivity {
 
+    @BindView(R.id.piechart_vp)
+    ViewPager mPiechartVp;
+    @BindView(R.id.pre_btn)
+    Button mPreBtn;
+    @BindView(R.id.next_btn)
+    Button mNextBtn;
     private ViewPager mViewPager;
     private final String mJson = "[{\"date\":\"2016年5月\",\"obj\":[{\"title\":\"外卖\",\"value\":34},{\"title\":\"娱乐\",\"value\":21},{\"title\":\"其他\",\"value\":45}]},{\"date\":\"2016年6月\",\"obj\":[{\"title\":\"苹果\",\"value\":16},{\"title\":\"栗子\",\"value\":4},{\"title\":\"其他\",\"value\":6}]},{\"date\":\"2016年7月\",\"obj\":[{\"title\":\"小木箱\",\"value\":23},{\"title\":\"小铁箱\",\"value\":24},{\"title\":\"其他\",\"value\":27}]}]";
     private ArrayList<MonthBean> mData;
@@ -25,6 +37,7 @@ public class PieChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart);
+        ButterKnife.bind(this);
         mViewPager = (ViewPager) findViewById(R.id.piechart_vp);
         initData();
         initView();
@@ -49,5 +62,34 @@ public class PieChartActivity extends AppCompatActivity {
                 return mData.size();
             }
         });
+        upDateJumpText();
+    }
+
+    private void upDateJumpText() {
+        CharSequence jumpNextText = mPiechartVp.getCurrentItem() != mPiechartVp.getAdapter().getCount() - 1 ? handleText(mData.get((mPiechartVp.getCurrentItem() + 1)).date) : "没有啦!";
+        CharSequence jumpPreText = mPiechartVp.getCurrentItem() != 0 ? handleText(mData.get((mPiechartVp.getCurrentItem() - 1)).date) : "没有啦!";
+        mNextBtn.setText(jumpNextText);
+        mPreBtn.setText(jumpPreText);
+    }
+
+    private CharSequence handleText(String date) {
+        return date.substring(date.indexOf("年") + 1);
+    }
+
+    @OnClick({R.id.piechart_vp, R.id.pre_btn, R.id.next_btn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.pre_btn:
+                if (mViewPager.getCurrentItem() != 0) {
+                    mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+                }
+                break;
+            case R.id.next_btn:
+                if (mViewPager.getCurrentItem() != mPiechartVp.getAdapter().getCount()) {
+                    mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
+                }
+                break;
+        }
+        upDateJumpText();
     }
 }
